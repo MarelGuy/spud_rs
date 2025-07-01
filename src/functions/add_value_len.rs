d06560b8dea211e5027f130149b280b28fb40a8c
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2025-07-01 20:24:43 +0200

    Refactor tests for add_value_length function to add u8, 16, u32, and u64

--- src/functions/add_value_len.rs
+++ src/functions/add_value_len.rs
@@ -22,20 +22,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add_value_length() {
-        let mut data: Vec<u8> = Vec::new();
+    fn test_add_value_length_u8() {
+        let mut data: Vec<u8> = Vec::with_capacity(1);
 
         add_value_length(&mut data, 42);
         assert_eq!(data, vec![SpudTypes::U8 as u8, 42]);
+    }
 
-        data.clear();
+    #[test]
+    fn test_add_value_length_u16() {
+        let mut data: Vec<u8> = Vec::with_capacity(2);
 
         add_value_length(&mut data, 256);
         assert_eq!(data, vec![SpudTypes::U16 as u8, 0, 1]);
+    }
 
-        data.clear();
+    #[test]
+    fn test_add_value_length_u32() {
+        let mut data: Vec<u8> = Vec::with_capacity(4);
 
         add_value_length(&mut data, 65536);
         assert_eq!(data, vec![SpudTypes::U32 as u8, 0, 0, 1, 0]);
+    }
+
+    #[test]
+    fn test_add_value_length_u64() {
+        let mut data: Vec<u8> = Vec::with_capacity(8);
+
+        add_value_length(&mut data, 4_294_967_296);
+        assert_eq!(data, vec![SpudTypes::U64 as u8, 0, 0, 0, 0, 1, 0, 0, 0]);
     }
 }
