573ba4c7cb1ec2a193f29e007fbf6d17a74e36fb
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2025-07-20 13:36:48 +0200

    Refactor lib.rs to ensure all modules are conditionally compiled based on 'sync' or 'async' features

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 #[cfg(all(not(feature = "async"), not(feature = "sync")))]
 compile_error!("You must enable either the 'async' or 'sync' feature (or both)!");
 
+#[cfg(any(feature = "sync", feature = "async"))]
 pub const SPUD_VERSION: &str = "SPUD-0.8.0";
 
+#[cfg(any(feature = "sync", feature = "async"))]
 pub mod types;
 
+#[cfg(any(feature = "sync", feature = "async"))]
 mod functions;
+
+#[cfg(any(feature = "sync", feature = "async"))]
 mod spud_builder;
+#[cfg(any(feature = "sync", feature = "async"))]
 mod spud_decoder;
+#[cfg(any(feature = "sync", feature = "async"))]
 mod spud_error;
+#[cfg(any(feature = "sync", feature = "async"))]
 mod spud_types;
 
 #[cfg(any(feature = "sync", feature = "async"))]
