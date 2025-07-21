b2649caf19a5ce3cd78a32442a3506dab9502d57
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2025-07-21 14:02:26 +0200

    added object decoding tests

--- src/spud_decoder/decoder_functions/object_start.rs
+++ src/spud_decoder/decoder_functions/object_start.rs
@@ -32,3 +32,112 @@ pub(crate) fn object_start(
 
     Ok(Value::Object(output_object))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[cfg(feature = "sync")]
+    #[test]
+    fn test_single_object() {
+        let builder = SpudBuilderSync::new();
+
+        builder
+            .object(|obj: &SpudObjectSync| {
+                obj.add_value("null", ())?;
+                Ok(())
+            })
+            .unwrap();
+
+        let encoded_bytes: Vec<u8> = builder.encode().unwrap();
+
+        let mut decoder: SpudDecoderSync = SpudDecoderSync::new(&encoded_bytes).unwrap();
+
+        decoder.decode(false, false).unwrap();
+    }
+
+    #[cfg(feature = "async")]
+    #[tokio::test]
+    async fn test_single_object_async() {
+        use std::sync::Arc;
+
+        use tokio::sync::{Mutex, MutexGuard};
+
+        let builder: SpudBuilderAsync = SpudBuilderAsync::new();
+
+        builder
+            .object(async |obj: Arc<Mutex<SpudObjectAsync>>| {
+                let obj: MutexGuard<'_, SpudObjectAsync> = obj.lock().await;
+
+                obj.add_value("null", ()).await?;
+                Ok(())
+            })
+            .await
+            .unwrap();
+
+        let encoded_bytes: Vec<u8> = builder.encode().await.unwrap();
+
+        let mut decoder: SpudDecoderSync = SpudDecoderSync::new(&encoded_bytes).unwrap();
+
+        decoder.decode(false, false).unwrap();
+    }
+
+    #[cfg(feature = "sync")]
+    #[test]
+    fn test_nested_object() {
+        let builder = SpudBuilderSync::new();
+
+        builder
+            .object(|obj: &SpudObjectSync| {
+                obj.add_value("null", ())?;
+
+                obj.object("object", |nested_obj: &SpudObjectSync| {
+                    nested_obj.add_value("null", ())?;
+                    Ok(())
+                })?;
+
+                Ok(())
+            })
+            .unwrap();
+
+        let encoded_bytes: Vec<u8> = builder.encode().unwrap();
+
+        let mut decoder: SpudDecoderSync = SpudDecoderSync::new(&encoded_bytes).unwrap();
+
+        decoder.decode(false, false).unwrap();
+    }
+
+    #[cfg(feature = "async")]
+    #[tokio::test]
+    async fn test_nested_object_async() {
+        use std::sync::Arc;
+
+        use tokio::sync::{Mutex, MutexGuard};
+
+        let builder: SpudBuilderAsync = SpudBuilderAsync::new();
+
+        builder
+            .object(async |obj: Arc<Mutex<SpudObjectAsync>>| {
+                let obj: MutexGuard<'_, SpudObjectAsync> = obj.lock().await;
+
+                obj.add_value("null", ()).await?;
+
+                obj.object("object", async |nested_obj: Arc<Mutex<SpudObjectAsync>>| {
+                    let nested_obj: MutexGuard<'_, SpudObjectAsync> = nested_obj.lock().await;
+
+                    nested_obj.add_value("null", ()).await?;
+                    Ok(())
+                }).await?;
+
+                Ok(())
+            })
+            .await
+            .unwrap();
+
+        let encoded_bytes: Vec<u8> = builder.encode().await.unwrap();
+
+        let mut decoder: SpudDecoderSync = SpudDecoderSync::new(&encoded_bytes).unwrap();
+
+        decoder.decode(false, false).unwrap();
+    }
+}
