0e4cb23b80f8113deb09f80fe6431a87509d94de
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2025-07-21 11:30:18 +0200

    added unit tests for Date

--- src/types/date.rs
+++ src/types/date.rs
@@ -21,32 +21,11 @@ impl Date {
     ///
     /// Returns an error if the month is not between 1 and 12, or if the day is not valid for the given month and year.
     pub fn new(year: u16, month: u8, day: u8) -> Result<Self, SpudError> {
-        if !(1..=12).contains(&month) {
-            return Err(SpudError::ValidationError(
-                "The month must be between 1 and 12".into(),
-            ));
-        }
+        let date: Date = Date { year, month, day };
 
-        let max_days: u8 = match month {
-            1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
-            4 | 6 | 9 | 11 => 30,
-            2 => {
-                if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) {
-                    29
-                } else {
-                    28
-                }
-            }
-            _ => unreachable!(),
-        };
+        date.check_validity()?;
 
-        if !(1..=max_days).contains(&day) {
-            return Err(SpudError::ValidationError(format!(
-                "Invalid day. The month {month} of the year {year} has {max_days} days."
-            )));
-        }
-
-        Ok(Date { year, month, day })
+        Ok(date)
     }
 
     pub(crate) fn as_le_bytes(self) -> Vec<u8> {
@@ -57,6 +36,36 @@ impl Date {
         bytes.push(self.day);
 
         bytes
+    }
+
+    fn check_validity(self) -> Result<(), SpudError> {
+        if !(1..=12).contains(&self.month) {
+            return Err(SpudError::ValidationError(
+                "The month must be between 1 and 12".into(),
+            ));
+        }
+
+        let max_days: u8 = match self.month {
+            1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+            4 | 6 | 9 | 11 => 30,
+            2 => {
+                if (self.year % 4 == 0 && self.year % 100 != 0) || (self.year % 400 == 0) {
+                    29
+                } else {
+                    28
+                }
+            }
+            _ => unreachable!(),
+        };
+
+        if !(1..=max_days).contains(&self.day) {
+            return Err(SpudError::ValidationError(format!(
+                "Invalid day. The month {} of the year {} has {} days.",
+                self.month, self.year, max_days
+            )));
+        }
+
+        Ok(())
     }
 }
 
@@ -105,7 +114,11 @@ impl FromStr for Date {
         let month: u8 = u8::from_str(parts[1]).map_err(|_| fmt::Error)?;
         let day: u8 = u8::from_str(parts[2]).map_err(|_| fmt::Error)?;
 
-        Ok(Date { year, month, day })
+        let date: Date = Date { year, month, day };
+
+        date.check_validity().map_err(|_| fmt::Error)?;
+
+        Ok(date)
     }
 }
 
@@ -125,5 +138,90 @@ impl TryFrom<Date> for NaiveDate {
 impl fmt::Display for Date {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:04}-{:02}-{:02}", self.year, self.month, self.day)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::NaiveTime;
+
+    use super::*;
+
+    #[test]
+    fn test_date_creation() {
+        let date: Result<Date, SpudError> = Date::new(2023, 3, 15);
+
+        assert!(date.is_ok());
+    }
+
+    #[test]
+    fn test_date_creation_invalid() {
+        let date: Result<Date, SpudError> = Date::new(2023, 13, 15);
+
+        assert!(date.is_err());
+
+        let date: Result<Date, SpudError> = Date::new(2023, 2, 30);
+
+        assert!(date.is_err());
+    }
+
+    #[test]
+    fn test_date_display() {
+        let date: Date = Date::new(2023, 3, 15).unwrap();
+
+        assert_eq!(date.to_string(), "2023-03-15");
+    }
+
+    #[test]
+    fn test_date_from_naive_date() {
+        let naive_date: NaiveDate = NaiveDate::from_ymd_opt(2023, 3, 15).unwrap();
+        let date: Result<Date, SpudError> = Date::try_from(naive_date);
+
+        assert!(date.is_ok());
+        assert_eq!(date.unwrap().to_string(), "2023-03-15");
+    }
+
+    #[test]
+    fn test_date_from_naive_date_time() {
+        let naive_date: NaiveDate = NaiveDate::from_ymd_opt(2023, 3, 15).unwrap();
+        let naive_time: NaiveTime = NaiveTime::from_hms_nano_opt(12, 30, 45, 500_000_000).unwrap();
+
+        let naive_datetime: NaiveDateTime = NaiveDateTime::new(naive_date, naive_time);
+
+        let date: Result<Date, SpudError> = Date::try_from(naive_datetime);
+
+        assert!(date.is_ok());
+        assert_eq!(date.unwrap().to_string(), "2023-03-15");
+    }
+
+    #[test]
+    fn test_date_from_str() {
+        let date_str: &str = "2023-03-15";
+        let date: Result<Date, fmt::Error> = Date::from_str(date_str);
+
+        assert!(date.is_ok());
+        assert_eq!(date.unwrap().to_string(), "2023-03-15");
+    }
+
+    #[test]
+    fn test_date_from_str_invalid() {
+        let invalid_date_str: &str = "2023-13-15";
+        let date: Result<Date, fmt::Error> = Date::from_str(invalid_date_str);
+
+        assert!(date.is_err());
+
+        let invalid_date_str: &str = "2023-02-30";
+        let date: Result<Date, fmt::Error> = Date::from_str(invalid_date_str);
+
+        assert!(date.is_err());
+    }
+
+    #[test]
+    fn test_date_to_naive_date() {
+        let date: Date = Date::new(2023, 3, 15).unwrap();
+        let naive_date: Result<NaiveDate, SpudError> = NaiveDate::try_from(date);
+
+        assert!(naive_date.is_ok());
+        assert_eq!(naive_date.unwrap().to_string(), "2023-03-15");
     }
 }
